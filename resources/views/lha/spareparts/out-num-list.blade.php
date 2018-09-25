4cc84eb5c2ea9606991710692dc9a748199719e6
sjzlai <sjzlai520@163.com>
2018-09-25 11:12:39 +0800

    9/25/1

--- resources/views/lha/spareparts/out-num-list.blade.php
+++ resources/views/lha/spareparts/out-num-list.blade.php
@@ -15,7 +15,6 @@
             {{--<th class="tc" width="5%"></th>--}}
             {{--<th class="tc">排序</th>--}}
             <th class="tc">出库单号</th>
-            <th>数量</th>
             <th>更新时间</th>
             <th>操作</th>
         </tr>
@@ -24,7 +23,6 @@
                 <tr>
                     {{--<td class="tc"><input type="checkbox" name="ckb" class="part" value="{{$value->out_storage_no}}" ></td>--}}
                     <td class="tc" >{{$value->out_storage_no}}</td>
-                    <td>{{$value->spare_number}}</td>
                     <td>{{$value->updated_at}}</td>
                     <td>
                         {{--<a href="{{url('ad/spare/outInfo/'.$value->part_id.'/'.$value->part_number)}}">出库</a>--}}
